49c700e2a9bade7298d88dc137963d4d39d23ed8
Tristan Stadler <donotpublish@my.email>
2025-06-01 11:16:45 +0200

    Repaired the cars.rs file so it compiles.
    Timer is not a component itself.

--- src/demo/cars.rs
+++ src/demo/cars.rs
@@ -6,7 +6,7 @@ use crate::{AppSystems, PausableSystems};
 pub(super) fn plugin(app: &mut App) {
     app.add_systems(
         FixedUpdate,
-        car_spawn
+        car_spawn_update
             .in_set(AppSystems::TickTimers)
             .in_set(PausableSystems),
     );
@@ -14,26 +14,31 @@ pub(super) fn plugin(app: &mut App) {
 
 
 #[derive(Component)]
-struct CarSpawner;
+struct CarSpawner {
+    timer: Timer
+}
+impl CarSpawner {
+    fn new(interval: Duration) -> CarSpawner {
+        CarSpawner {
+            timer: Timer::new(interval, TimerMode::Repeating)
+        }
+    }
+}
 
 pub fn car_spawner(position: Vec3, interval: Duration) -> impl Bundle {
-
     (
         Name::new("car spawner"),
-        CarSpawner,
-        Timer::new(interval, TimerMode::Repeating),
+        CarSpawner::new(interval),
         Transform::from_translation(position),
     )
 }
 
-fn car_spawn(time: Res<Time>, mut car_spawner_query: Query< &CarSpawner>) {
+fn car_spawn_update(time: Res<Time>, car_spawner_query: Query<&mut CarSpawner>) {
     for mut car_spawner in car_spawner_query {
         car_spawner.timer.tick(time.delta());
+
+        if car_spawner.timer.just_finished() {
+            println!("Spawn car");
+        }
     }
-
-    if timer.0.just_finished() {
-        println!("Spawn car");
-    }
-
-
 }
