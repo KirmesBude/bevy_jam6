f75bee77cbe665cc7928b18f14ca41d865eed53c
freakywaves <dolflou032@gmail.com>
2025-06-03 22:57:17 +0200

    add road assets

--- src/game/road.rs
+++ src/game/road.rs
@@ -1,1 +1,27 @@
+use bevy::prelude::*;
 
+#[derive(Debug, Default, Component, Reflect)]
+pub struct Road;
+
+pub(super) fn plugin(app: &mut App) {
+    app.register_type::<Road>();
+
+    app.register_type::<RoadAssets>();
+    app.load_resource::<RoadAssets>();
+}
+
+#[derive(Resource, Asset, Clone, Reflect)]
+#[reflect(Resource)]
+pub struct RoadAssets {
+    #[dependency]
+    pub road_straight: Handle<SceneRoot>,
+}
+
+impl FromWorld for RoadAssets {
+    fn from_world(world: &mut World) -> Self {
+        let assets = world.resource::<AssetServer>();
+        Self {
+            road_straight: assets.load("models/kenney_city-kit-roads/road-straight.glb"),
+        }
+    }
+}
