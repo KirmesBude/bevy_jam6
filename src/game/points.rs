8fd7964258b1acf355cf7734d81724dc8128884b
KirmesBude <kirmesbude@gmail.com>
2025-06-08 17:00:19 +0200

    Add CarCrash Event

--- src/game/points.rs
+++ src/game/points.rs
@@ -1,11 +1,12 @@
-use avian3d::prelude::*;
 use bevy::prelude::*;
 
-use crate::game::car::Car;
+use crate::game::car::CarCrash;
 
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<HighScore>();
     app.init_resource::<HighScore>();
+
+    app.add_systems(Update, update_highscore);
 }
 
 const CAR_COLLISION_MULTIPLIER: f32 = 100.;
@@ -21,33 +22,8 @@ impl HighScore {
     }
 }
 
-/// To be added to a car entity
-/// Will observer all car related collisions and update score based on total collision impulse
-pub fn car_observer_update_highscore(
-    trigger: Trigger<OnCollisionStart>,
-    mut high_score: ResMut<HighScore>,
-    mut cars: Query<&mut Car>,
-    collisions: Collisions,
-) {
-    let car = trigger.target();
-    let other_entity = trigger.collider;
-
-    /* Lets say we only care about car on car collision for the points */
-    if !cars.contains(other_entity) {
-        return;
-    }
-    // dbg!("Colliding");
-
-    if let Some(contact_pair) = collisions.get(car, other_entity) {
-        let [mut car1, mut car2] = cars.get_many_mut([car, other_entity]).unwrap();
-
-        if !car1.wrecked || !car2.wrecked {
-            car1.wrecked = true;
-            car2.wrecked = true;
-
-            high_score.0 +=
-                contact_pair.total_normal_impulse().element_sum() * CAR_COLLISION_MULTIPLIER;
-            //dbg!("Increase high score to {}", high_score);
-        }
+fn update_highscore(mut car_crashes: EventReader<CarCrash>, mut high_score: ResMut<HighScore>) {
+    for car_crash in car_crashes.read() {
+        high_score.0 += car_crash.magnitude * CAR_COLLISION_MULTIPLIER;
     }
 }
